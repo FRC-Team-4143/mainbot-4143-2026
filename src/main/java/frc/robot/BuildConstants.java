7c483158601cb03643a3284054727a2620dbc644
Ethan Tolan <bassjupiter3232@gmail.com>
2026-01-12 18:10:51 -0600

    converted to 2026

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "mainbot-4143-2026";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "27005a2838c3a060a04f03e737cdb26187c0994b";
+  public static final String GIT_DATE = "2026-01-05 16:53:24 CST";
+  public static final String GIT_BRANCH = "master";
+  public static final String BUILD_DATE = "2026-01-12 18:10:01 CST";
+  public static final long BUILD_UNIX_TIME = 1768263001546L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
