0c5f86c74edc090b2dcfda108943bc2f150d40e2
Adric Schonert <adric.schonert@gmail.com>
2026-01-29 19:28:48 -0600

    added states constants

--- src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
@@ -1,5 +1,20 @@
 package frc.robot.subsystems.Climber;
 
-public class ClimberConstants {
+import com.marswars.subsystem.MwConstants;
+
+public class ClimberConstants extends MwConstants {
     
+    public enum ClimberStates {
+        IDLE_FINALE,
+        IDLE_AUTO,
+        STORED,
+        STORING,
+        EXTENDING,
+        DEPLOYED,
+        ENGAGE,
+        AUTO_CLIMB_UP,
+        AUTO_CLIMB_DOWN,
+        TELEOP_CLIMB,
+        DISENGAGE,
+    }
 }
