2f64378c94fea166cf79c0233e56229ad826ff77
Adric Schonert <adric.schonert@gmail.com>
2026-01-31 14:25:06 -0600

    made state machine and started transistions

--- src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
@@ -1,20 +1,61 @@
 package frc.robot.subsystems.Climber;
 
+import static edu.wpi.first.units.Units.Meter;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.marswars.subsystem.MwConstants;
+import com.marswars.util.FxMotorConfig;
+import com.marswars.util.FxMotorConfig.FxMotorType;
+
+import edu.wpi.first.units.Units;
+
+// IMPORTANT
+// Change ALL placeholders BEFORE branch merge
+// Delete all Place Holders
 
 public class ClimberConstants extends MwConstants {
-    
     public enum ClimberStates {
-        IDLE_FINALE,
-        IDLE_AUTO,
         STORED,
-        STORING,
         EXTENDING,
         DEPLOYED,
-        ENGAGE,
+        TELEOP_ENGAGE,
+        TELEOP_CLIMB_UP,
+        IDLE_FINALE,
+        TELEOP_CLIMB_DOWN,
+        AUTO_ENGAGE,
         AUTO_CLIMB_UP,
+        IDLE_AUTO,
         AUTO_CLIMB_DOWN,
-        TELEOP_CLIMB,
         DISENGAGE,
+        STORING
+    }
+
+    //Extender configs
+    public final int  EXTENDERMOTER_ID = 1; //Place holder
+    public final boolean EXTENDERMOTER_INVERTED = false; //Place Holder
+    public final double EXTENDER_GEAR_RATIO = 1.0; //place holder
+    public final FxMotorConfig EXTENDER_MOTOR_CONFIG = new FxMotorConfig();
+
+    //Arm configs
+    public final int  ARM_MOTER_ID = 1; //Place holder
+    public final boolean ARM_MOTER_INVERTED = false; //Place Holder
+    public final double ARM_GEAR_RATIO = 1.0; //place holder
+    public final FxMotorConfig ARM_MOTOR_CONFIG = new FxMotorConfig();
+    public final double ARM_LENGTH = 1.0; // Meters, Place Holder
+    public final double ARM_MASS = 1.0; // KG, Place Holder
+    public final double ARM_MIN_ANGLE = 1.0; // Radians, Place Holder
+    public final double ARM_MAX_ANGLE = 2.0; // Radians, Place Holder
+
+    public ClimberConstants() {
+        // Configure Indexer Motor
+        EXTENDER_MOTOR_CONFIG.can_id = EXTENDERMOTER_ID;
+        EXTENDER_MOTOR_CONFIG.motor_type = FxMotorType.FALCON500; //Place Holder
+        EXTENDER_MOTOR_CONFIG.canbus_name = "CANivore"; // Place Holder
+        EXTENDER_MOTOR_CONFIG.config = new TalonFXConfiguration(); // Place Holder
+
+        ARM_MOTOR_CONFIG.can_id = ARM_MOTER_ID; // Place Holder
+        ARM_MOTOR_CONFIG.motor_type = FxMotorType.X60; // Place Holder
+        ARM_MOTOR_CONFIG.canbus_name = "rio"; // Place Holder
+        ARM_MOTOR_CONFIG.config = new TalonFXConfiguration(); // Place Holder
     }
 }
--- src/main/java/frc/robot/subsystems/Climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberSubsystem.java
@@ -1,5 +1,117 @@
 package frc.robot.subsystems.Climber;
 
-public class ClimberSubsystem {
-    
+import java.util.Arrays;
+import java.util.List;
+
+import com.marswars.mechanisms.RollerMech;
+import com.marswars.mechanisms.ArmMech;
+import com.marswars.subsystem.MwSubsystem;
+import com.marswars.subsystem.SubsystemIoBase;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.Climber.ClimberConstants.ClimberStates;
+
+public class ClimberSubsystem extends MwSubsystem<ClimberStates, ClimberConstants> {
+    private static ClimberSubsystem instance_ = null;
+
+    // temporary varibale for state machine transstions
+    private static boolean teleop_climb_state_enabled_ = false;
+    private static boolean teleop_climb_state_canceled_ = false;
+    private static boolean auto_climb_state_enabled_ = false;
+    private static boolean all_other_attachments_retracted_ = false;
+    private static boolean extended_ = false;
+    private static boolean finished_deploying_ = false;
+    private static boolean robot_allined_and_ready_ = false;
+    private static boolean engaged_ = false;
+    private static boolean finished_teleop_climb_up_ = false;
+    private static boolean finished_teleop_climb_down_ = false;
+    private static boolean finshed_auto_clim_up_ = false;
+    private static boolean down_climb_state_enabled_ = false;
+    private static boolean finished_auto_climb_down_ = false;
+
+    public static ClimberSubsystem getInstance() {
+        if (instance_ == null) {
+            instance_ = new ClimberSubsystem();
+        }
+        return instance_;
+    }
+
+    private RollerMech Extender;
+    private ArmMech Arm;
+
+    // climer constructor
+    public ClimberSubsystem() {
+        super(ClimberStates.STORED, new ClimberConstants());
+        Extender = new RollerMech(
+                getSubsystemKey(),
+                "Extender",
+                List.of(CONSTANTS.EXTENDER_MOTOR_CONFIG),
+                CONSTANTS.EXTENDER_GEAR_RATIO);
+        Arm = new ArmMech(
+                getSubsystemKey(),
+                "Arm",
+                List.of(CONSTANTS.ARM_MOTOR_CONFIG),
+                CONSTANTS.ARM_GEAR_RATIO,
+                CONSTANTS.ARM_LENGTH,
+                CONSTANTS.ARM_MASS,
+                CONSTANTS.ARM_MAX_ANGLE,
+                CONSTANTS.ARM_MIN_ANGLE);
+    }
+
+    // state machine
+    @Override
+    public void updateLogic(double timestamp) {
+        switch (system_state_) {
+            case STORED:
+                break;
+            case EXTENDING:
+                break;
+            case DEPLOYED:
+                break;
+            case TELEOP_ENGAGE:
+                break;
+            case TELEOP_CLIMB_UP:
+                break;
+            case IDLE_FINALE:
+                break;
+            case TELEOP_CLIMB_DOWN:
+                break;
+            case AUTO_ENGAGE:
+                break;
+            case AUTO_CLIMB_UP:
+                break;
+            case IDLE_AUTO:
+                break;
+            case AUTO_CLIMB_DOWN:
+                break;
+            case DISENGAGE:
+                break;
+            case STORING:
+                break;
+        }
+    }
+
+    public void handleStateTransistion(ClimberStates wanted) {
+        if (system_state_ == ClimberStates.STORED && all_other_attachments_retracted_ && (teleop_climb_state_enabled_ || auto_climb_state_enabled_)) {
+            system_state_ = ClimberStates.EXTENDING;
+        } else {
+        } //no other actions
+        if (system_state_ == ClimberStates.EXTENDING && extended_) {
+            system_state_ = ClimberStates.DEPLOYED;
+        } else if (system_state_ == ClimberStates.EXTENDING && teleop_climb_state_canceled_) {
+            system_state_ = ClimberStates.STORING;
+        } else {
+        } // no other actions
+
+    }
+
+    @Override
+    public List<SubsystemIoBase> getIos() {
+        return Arrays.asList(Extender, Arm);
+    }
+
+    @Override
+    public void reset() {
+        system_state_ = ClimberStates.STORED;
+    }
 }
