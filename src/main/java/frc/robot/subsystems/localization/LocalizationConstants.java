0d7807453856db3a3e123b2d354b6011165e73d5
ColeHunt <huntcr2001@gmail.com>
2026-02-01 17:37:46 -0600

    adds optional noise gen to swerve odom measurements

--- src/main/java/frc/robot/subsystems/localization/LocalizationConstants.java
+++ src/main/java/frc/robot/subsystems/localization/LocalizationConstants.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.localization;
 
 import com.marswars.subsystem.MwConstants;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
@@ -8,10 +10,14 @@ public class LocalizationConstants extends MwConstants {
 
     // Subsystem States
     public enum LocalizationStates {
-        ACTIVE
+        ODOM_ONLY,
+        VISION_SIM,
+        FULL
     }
 
     public final Pose2d START_POSE = new Pose2d(3.0, 3.0, Rotation2d.kZero);
+    public final AprilTagFieldLayout APRIL_TAG_LAYOUT =
+            AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded);
 
     public LocalizationConstants() {
         // Some constants require dynamic initialization like through the JSON loader
