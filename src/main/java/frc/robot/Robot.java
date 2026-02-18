cecc64a1a5f5339ed6552ec54949cee21cc3e1d9
Ben Shoff <benshoff100@gmail.com>
2026-02-17 19:43:49 -0600

    Added right side auto

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.autos.Left_Start_Neutral_Outpost_Climb;
+import frc.robot.autos.Right_Start_Neutral_Depot_Climb;
 import frc.robot.lib2026.FieldConstants;
 import frc.robot.lib2026.FieldRegions;
 import frc.robot.lib2026.FieldTargets;
@@ -46,7 +47,8 @@ public class Robot extends TimedRobot {
         AutoManager.getInstance()
                 .registerAutos(
                         // Add your auto routines here as you create them
-                        new Left_Start_Neutral_Outpost_Climb());
+                        new Left_Start_Neutral_Outpost_Climb(),
+                        new Right_Start_Neutral_Depot_Climb());
 
         // Set the default target for the shooter to be the hub
         ShooterSubsystem.getInstance().setTarget(FieldTargets.Shooter.HUB);
