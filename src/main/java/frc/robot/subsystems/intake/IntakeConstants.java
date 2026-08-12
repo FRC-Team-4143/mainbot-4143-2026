cc9f8a6cc04d7ce91f2d627ab4ab74fa50108a9b
Eli Cole <epcole11@gmail.com>
2026-08-12 18:27:18 -0500

    Update at 'Wed Aug 12 18:27:18 CDT 2026'

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -82,7 +82,7 @@ public class IntakeConstants extends MwConstants {
     public final double PIVOT_SQUEEZE_CURRENT = 15.0;
     public final double PIVOT_SQUEEZE_MAX_POSITION = Units.degreesToRadians(80);
     public final double PIVOT_SQUEEZE_HOLD_POSITION = Units.degreesToRadians(80);
-    public final Slot2Configs PIVOT_CURRENT_GAINS = new Slot2Configs().withKP(0.0).withKI(0.0166);
+    public final Slot2Configs PIVOT_CURRENT_GAINS = new Slot2Configs().withKP(0.01).withKI(0.06);
 
     // Homing for pivot - drive with a small duty cycle until the motor current spikes
     public final double PIVOT_HOMING_DUTY_CYCLE = -0.15;
